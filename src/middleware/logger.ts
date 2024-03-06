63d78a25f106d23413101fd4f8080c8ffac80a04
Thunman <Thunman42@gmail.com>
2024-03-06 20:33:19 +0100

    adding a logger

---
+++ src/middleware/logger.ts
@@ -0,0 +1,30 @@
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+import { Request } from "express";
+
+const logDir = path.join(__dirname, "../../logs");
+fs.mkdirSync(logDir, { recursive: true });
+const logStream = fs.createWriteStream(path.join(logDir, "logs.log"), {
+	flags: "a",
+});
+
+const logger = morgan(
+	function (tokens, req, res) {
+		const reqBody = { ...(req as Request).body }; // Create a copy of the request body
+		if (reqBody.password) {
+			reqBody.password = "REDACTED"; // Redact the password
+		}
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, "content-length"),
+			"-",
+			tokens["response-time"](req, res),
+			"ms",
+			JSON.stringify((req as Request).body),
+		].join(" ");
+	},
+	{ stream: logStream }
+);
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { userRouter } from "./routes/routes";
+import { logger } from "./middleware/logger";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve("/home/thunman/myHomeFrontend/dist")));
 app.use(express.json());
+app.use(logger);
+app.use(express.static(path.resolve("/home/thunman/myHomeFrontend/dist")));
 app.use("/api/users", userRouter);
 app.get("*", (req, res) => {
 	res.sendFile(path.resolve("/home/thunman/myHomeFrontend/dist/index.html"));
