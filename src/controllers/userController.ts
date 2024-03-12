5d683d53df41ea12c6ee05c3520172450c0f075e
Thunman <Thunman42@gmail.com>
2024-03-12 16:07:04 +0100

    fixing a bug that caused the server to crash due to the hibernate command not being async

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,182 +2,185 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import path from "path";
 import fs from "fs";
-import { exec as execCb, execSync } from "child_process";
+import { exec, execSync } from "child_process";
 import { wake } from "wol";
 import Docker from "dockerode";
-import { promisify } from "util";
+import ping from "ping";
+
 const docker = new Docker();
 
 export const userController = {
-	login: async (req: Request, res: Response) => {
-		try {
-			const { username, password } = req.body;
-			if (
-				username === process.env.USERNAME &&
-				password === process.env.PASSWORD
-			) {
-				const token = jwt.sign({ username }, process.env.JWT_SECRET, {
-					expiresIn: "1h",
-				});
-				res.cookie("token", token, {
-					httpOnly: true,
-					secure: true,
-					sameSite: "none",
-				});
-				res.cookie("loggedIn", true);
-				res.status(200).json({
-					success: true,
-					message: "Success",
-					data: {},
-				});
-			} else {
-				res.status(401).json({
-					success: false,
-					message: "Errorcode 2",
-					data: {},
-				});
-			}
-		} catch (error) {
-			console.error(error);
-			res.status(400).json({
-				success: false,
-				message: "Errorcode: 2",
-				data: {},
-			});
-		}
-	},
+  login: async (req: Request, res: Response) => {
+    try {
+      const { username, password } = req.body;
+      if (
+        username === process.env.USERNAME &&
+        password === process.env.PASSWORD
+      ) {
+        const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        });
+        res.cookie("loggedIn", true);
+        res.status(200).json({
+          success: true,
+          message: "Success",
+          data: {},
+        });
+      } else {
+        res.status(401).json({
+          success: false,
+          message: "Errorcode 2",
+          data: {},
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({
+        success: false,
+        message: "Errorcode: 2",
+        data: {},
+      });
+    }
+  },
+  getLogs: async (req: Request, res: Response) => {
+    try {
+      const filePath = path.resolve(__dirname, "../../logs/logs.log");
+      if (fs.existsSync(filePath)) {
+        const logFile = fs.readFileSync(filePath, "utf8");
+        res.json({
+          success: true,
+          message: "Log file fetched successfully",
+          data: { logs: logFile },
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "Log file not found.",
+          data: {},
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        message: "An error occurred while fetching the logs.",
+        data: {},
+      });
+    }
+  },
+  getStatus: async (req: Request, res: Response) => {
+    try {
+      const container = docker.getContainer("mongodb");
+      const mongo = await container.inspect();
+      const isMongoRunning = mongo.State.Running;
 
-	getLogs: async (req: Request, res: Response) => {
-		try {
-			const filePath = path.resolve(__dirname, "../../logs/logs.log");
-			if (fs.existsSync(filePath)) {
-				const logFile = fs.readFileSync(filePath, "utf8");
-				res.json({
-					success: true,
-					message: "Log file fetched successfully",
-					data: { logFile },
-				});
-			} else {
-				res.status(404).json({
-					success: false,
-					message: "Log file not found.",
-					data: {},
-				});
-			}
-		} catch (error) {
-			console.error(error);
-			res.status(500).json({
-				success: false,
-				message: "An error occurred while fetching the logs.",
-				data: {},
-			});
-		}
-	},
+      ping.sys.probe("192.168.50.135", function (isPCRunning) {
+        res.status(200).json({
+          success: true,
+          message: "Here you go",
+          data: { mongo: isMongoRunning, pc: isPCRunning },
+        });
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        message: "Error getting status",
+        data: {},
+      });
+    }
+  },
+  stopMongo: async (req: Request, res: Response) => {
+    try {
+      const container = await docker.getContainer("mongodb");
+      container.stop((error) => {
+        if (!error)
+          res.status(200).json({
+            success: true,
+            message: "Mongo stopped",
+            data: {},
+          });
+        else throw new Error();
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Error stoping",
+        data: error,
+      });
+    }
+  },
+  startMongo: async (req: Request, res: Response) => {
+    try {
+      const container = await docker.getContainer("mongodb");
+      container.start((error) => {
+        if (!error)
+          res.status(200).json({
+            success: true,
+            message: "Mongo started",
+            data: {},
+          });
+        else throw new Error();
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Error starting",
+        data: error,
+      });
+    }
+  },
 
-	getStatus: async (req: Request, res: Response) => {
-		const exec = promisify(execCb);
-		try {
-			const container = docker.getContainer("mongodb");
-			const mongo = await container.inspect();
-			const isMongoRunning = mongo.State.Running;
-			const { stdout } = await exec("ping -c 1 192.168.50.135");
-			const isPCRunning = stdout.includes(
-				"1 packets transmitted, 1 received"
-			);
-			res.status(200).json({
-				success: true,
-				message: "Here you go",
-				data: { isMongoRunning, isPCRunning },
-			});
-		} catch (error) {
-			res.status(500).json({
-				success: false,
-				message: "Error getting status",
-				data: {},
-			});
-		}
-	},
-	stopMongo: async (req: Request, res: Response) => {
-		try {
-			const container = await docker.getContainer("mongodb");
-			container.stop((error) => {
-				if (!error)
-					res.status(200).json({
-						success: true,
-						message: "Mongo stopped",
-						data: {},
-					});
-				else throw new Error();
-			});
-		} catch (error) {
-			res.status(500).json({
-				success: false,
-				message: "Error stoping",
-				data: error,
-			});
-		}
-	},
-	startMongo: async (req: Request, res: Response) => {
-		try {
-			const container = await docker.getContainer("mongodb");
-			container.start((error) => {
-				if (!error)
-					res.status(200).json({
-						success: true,
-						message: "Mongo started",
-						data: {},
-					});
-				else throw new Error();
-			});
-		} catch (error) {
-			res.status(500).json({
-				success: false,
-				message: "Error starting",
-				data: error,
-			});
-		}
-	},
+  hibernatePC: async (req: Request, res: Response) => {
+    try {
+      const command = `sshpass -p ${process.env.SSH_PASSWORD} ssh ${process.env.USERNAME}@192.168.50.135 "shutdown -h"`;
+      exec(command, (error) => {
+        if (error) {
+          throw new Error();
+        }
+      });
+      res.json({ success: true, message: "PC is hibernating", data: {} });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        message: "Error hibernating PC",
+        data: {},
+      });
+    }
+  },
 
-	hibernatePC: async (req: Request, res: Response) => {
-		try {
-			const command = `sshpass -p ${process.env.SSH_PASSWORD} ssh ${process.env.USERNAME}@192.168.50.135 "shutdown -h"`;
-			execSync(command);
-			res.json({ success: true, message: "PC is hibernating", data: {} });
-		} catch (error) {
-			res.status(500).json({
-				success: false,
-				message: "Error hibernating PC",
-				data: {},
-			});
-		}
-	},
+  wakePC: async (req: Request, res: Response) => {
+    wake("9C-5C-8E-87-CE-D0", (error) => {
+      if (error) {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ success: false, message: `${error}`, data: {} });
+      } else {
+        return res
+          .status(200)
+          .json({ success: true, message: "Success", data: {} });
+      }
+    });
+  },
 
-	wakePC: async (req: Request, res: Response) => {
-		wake("9C-5C-8E-87-CE-D0", (error) => {
-			if (error) {
-				console.error(error);
-				return res
-					.status(500)
-					.json({ success: false, message: `${error}`, data: {} });
-			} else {
-				return res
-					.status(200)
-					.json({ success: true, message: "Success", data: {} });
-			}
-		});
-	},
-
-	logout: async (req: Request, res: Response) => {
-		try {
-			res.clearCookie("token");
-			res.clearCookie("loggedIn");
-			res.status(200).json({
-				success: true,
-				message: "logged out",
-				data: {},
-			});
-		} catch {
-			res.status(500).json({ success: false, message: "error", data: {} });
-		}
-	},
+  logout: async (req: Request, res: Response) => {
+    try {
+      res.clearCookie("token");
+      res.clearCookie("loggedIn");
+      res.status(200).json({
+        success: true,
+        message: "logged out",
+        data: {},
+      });
+    } catch {
+      res.status(500).json({ success: false, message: "error", data: {} });
+    }
+  },
 };
--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -6,26 +6,28 @@ import { Request } from "express";
 const logDir = path.join(__dirname, "../../logs");
 fs.mkdirSync(logDir, { recursive: true });
 const logStream = fs.createWriteStream(path.join(logDir, "logs.log"), {
-	flags: "a",
+  flags: "a",
 });
 
 export const logger = morgan(
-	(tokens, req, res) => {
-		const reqBody = { ...(req as Request).body };
-		if (reqBody.password) {
-			reqBody.password = "REDACTED";
-		}
+  (tokens, req, res) => {
+    if (tokens.url(req, res) === "/api/users/login") {
+      const reqBody = { ...(req as Request).body };
+      if (reqBody.password) {
+        reqBody.password = "REDACTED";
+      }
 
-		return [
-			tokens.method(req, res),
-			tokens.url(req, res),
-			tokens.status(req, res),
-			tokens.res(req, res, "content-length"),
-			"-",
-			tokens["response-time"](req, res),
-			"ms",
-			JSON.stringify(reqBody),
-		].join(" ");
-	},
-	{ stream: logStream }
+      return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, "content-length"),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+        JSON.stringify(reqBody),
+      ].join(" ");
+    }
+  },
+  { stream: logStream }
 );
